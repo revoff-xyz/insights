b78b03b93d2664d2ef9d881e8e89803f533175fb
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-03-09 09:39:21 +0100

    Initial commit.

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.dahlqvistsandstrom)
+        return;
+    
+    window.dahlqvistsandstrom = {};
+    // TODO
+});
